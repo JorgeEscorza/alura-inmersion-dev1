c9f5005c2287847575dcfb8983629ebbece89150
jorge escorza <jorgezulan@gmail.com>
2024-09-17 22:34:37 -0600

    mas caracteres, boton limpiar y nivel de seguridad

--- inmersion-dev-aula1-Proyecto-Base/main.js
+++ inmersion-dev-aula1-Proyecto-Base/main.js
@@ -1,14 +1,15 @@
 let cantidad = document.getElementById("cantidad");
 let boton = document.getElementById("generar");
 let contrasena = document.getElementById("contrasena");
+let seguridad = document.getElementById("seguridad");
 
-const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
 function generar(){
 
     let numeroDigitado = parseInt(cantidad.value);
 
-    if (numeroDigitado < 8){
+    if (numeroDigitado < 8 || cantidad.value == ""){
         alert("La cantidad de caracteres tiene que ser al menos 8 ");
         return;
     }
@@ -23,5 +24,41 @@ function generar(){
     }
 
     contrasena.value = password;
+
+    let nivelSeguridad = calcularSeguridad(contrasena.value);
+
+    if (nivelSeguridad == 4){
+        seguridad.innerHTML = "La contraseña es fuerte";
+    }else if (nivelSeguridad < 4 && nivelSeguridad > 1){
+        seguridad.innerHTML = "La contraseña es medianamente fuerte";
+    }else if (nivelSeguridad < 2){
+        seguridad.innerHTML = "La contraseña es débil";
+    }
+
+}
+
+function limpiar(){
+    contrasena.value = "";
+    cantidad.value = "";
+    seguridad.innerHTML = "";
+}
+
+function calcularSeguridad(contrasena){
+
+    let contador = 0;
+
+    if (/[A-Z]/.test(contrasena)){
+        contador++;
+    }if (/[a-z]/.test(contrasena)){
+        contador++;
+    }if (/[0-9]/.test(contrasena)){
+        contador++;
+    }if (/[!"#$%&\'()*+,-./:;<=>?@\[\\\]^_`{|}~']/.test(contrasena)){
+        contador++;
+    }
+
+    console.log(contador);
+
+    return contador;
 
 }
