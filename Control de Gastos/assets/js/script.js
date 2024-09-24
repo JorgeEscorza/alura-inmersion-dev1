91244a73314affb38dff853daf832f127ac6b0d4
jorge escorza <jorgezulan@gmail.com>
2024-09-24 03:50:00 -0600

    campo de descripcion

--- Control de Gastos/assets/js/script.js
+++ Control de Gastos/assets/js/script.js
@@ -1,18 +1,21 @@
 let listaNombresGastos = [];
 let listaValorGastos = [];
+let listaDescripciones = [];
 
 // Esta función se invoca al momento de que el usuario hace clic en el botón
 function clickBoton(){
 
     let nombreGasto = document.getElementById("nombreGasto").value;
     let valorGasto = document.getElementById("valorGasto").value;
+    let descripcion = document.getElementById("descripcion").value;
+
     const total = document.getElementById("totalGastos");
     const contenedorTotal = document.getElementById("contenedorTotal");
 
     if (Number(total.innerText) + Number(valorGasto) < 150){
-
         listaNombresGastos.push(nombreGasto);
         listaValorGastos.push(valorGasto);
+        listaDescripciones.push(descripcion);
         actualizarListaGastos();
     }else{
         contenedorTotal.innerHTML += "<br> Límite de gastos alcanzado";
@@ -24,12 +27,14 @@ function actualizarListaGastos(){
 
     let htmlLista = "";
     const listaElementos = document.getElementById("listaDeGastos");
-    const totalElementos = document.getElementById("totalGastos");
+    //const totalElementos = document.getElementById("totalGastos");
     let totalGastos = 0;
 
     listaNombresGastos.forEach((elemento, posicion) => {
         const valorGasto = listaValorGastos[posicion];
+        const descripcion = listaDescripciones[posicion];
         htmlLista += `<li>${elemento} - USD ${Number(valorGasto).toFixed(2)}
+                        <br>${descripcion}<br>
                         <button onclick="eliminarGasto(${posicion});">Eliminar</button>
                     </li>`
 
@@ -50,6 +55,7 @@ function actualizarListaGastos(){
 function limpiar(){
     document.getElementById("nombreGasto").value = "";
     document.getElementById("valorGasto").value = "";
+    document.getElementById("descripcion").value = "";
 }
 
 function eliminarGasto(posicion){
@@ -59,3 +65,18 @@ function eliminarGasto(posicion){
     actualizarListaGastos();
 
 }
+
+function autoResize(textarea){
+
+    // Reseteamos la altura para calcular el nuevo tamaño.
+    textarea.style.height = "auto";
+
+    // Ajusta la altura en función del contenido.
+    textarea.style.height = textarea.scrollHeight + "px";
+
+    
+}
+
+const textareaField = document.getElementById("descripcion");
+textareaField.addEventListener("input", () => autoResize(textareaField));
+autoResize(textareaField);
